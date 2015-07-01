811b1f0de317827c64e8b0709f259b886c0b25de
huzy123 <309999134@qq.com>
2015-07-01 21:14:47 +0800

    后台模板

--- qdRSCompanyWebsite/src/resource.js
+++ qdRSCompanyWebsite/src/resource.js
@@ -9,12 +9,13 @@ document.write('<script src="../libs/angular/angular-sanitize.min.js"></script>'
 document.write('<script src="../libs/adminLTE/bootstrap/js/bootstrap.min.js"></script>');
 <!-- Bootstrap 3.3.4 -->
 document.write('<link rel="stylesheet" href="../libs/adminLTE/bootstrap/css/bootstrap.min.css"/>');
+document.write('<link rel="stylesheet" href="../libs/ionicons.min.css"/>');
 <!-- Theme style -->
 document.write('<link rel="stylesheet" href="../libs/adminLTE/dist/css/AdminLTE.min.css"/>');
 <!-- iCheck -->
 document.write('<link rel="stylesheet" href="../libs/adminLTE/plugins/iCheck/square/blue.css"/>');
 <!-- Font Awesome Icons -->
 document.write('<link rel="stylesheet" href="../libs/font-awesome.min.css"/>');
-
+document.write('<link rel="stylesheet" href="../libs/adminLTE/dist/css/skins/skin-blue.min.css"/>');
 document.write('<script src="../views/js/pubjs/getTime.js"></script>');
 
